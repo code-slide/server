285100ae9989bda35cf7d1a6e83840d9b6ef0c12
Do Duc Quan <99700700+ducquando@users.noreply.github.com>
2024-05-03 16:12:35 +0700

    Add version check and test case for library imports

--- ui.sh
+++ ui.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ "${ENV}" = 'DEV' ]; then 
-    cd ./dist && git clone -b features https://github.com/code-slide/ui.git
+    cd ./dist && git clone -b seo https://github.com/code-slide/ui.git
     cd ui && echo "VITE_SERVER_URL=/." > .env
     rm -rf package-lock.json
     npm install && npm run build
